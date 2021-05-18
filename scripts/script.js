09c18699164e8c9adeda8cda5de20608e3c123df
Aman Aggarwal <akaggarw@ucsd.edu>
2021-05-18 11:55:06 -0700

    Add serviceWorjer script

--- scripts/script.js
+++ scripts/script.js
@@ -38,3 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// Register the service worker
+if ('serviceWorker' in navigator){
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js')
+      .then(
+          (registration) => console.log("ServiceWorker registration successfull with scope: ", registration.scope),
+          (error) => console.log("ServiceWorker registration failed: ", error)
+      )
+  })
+}
+
--- sw.js
+++ sw.js
@@ -4,3 +4,50 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+
+const CACHE_NAME = 'journal-cache-v1';
+const urlsToCache = [
+    './', 
+    './style.css',
+    './index.html',
+    './scripts/script.js',
+    './scripts/router.js',
+    './components/entry-page.js',
+    './components/journal-entry.js',
+]  
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+        .then((response) => {
+            if (response) {
+                return response
+            }
+            return fetch(e.request).then(
+                (response) => {
+                    if (!response || response.status !== 200) {
+                        return response;
+                    }
+                    let responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                    .then((cache) => cache.put(e.request, responseToCache))
+                    return response;
+                }
+            )
+        })
+
+    )
+})
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
+})
